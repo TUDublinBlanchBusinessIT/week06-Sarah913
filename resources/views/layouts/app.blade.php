7f1f37c41cf4459dbaae8a0ef32300e768daee45
Sarah Forde <you@example.com>
2026-03-09 16:54:23 +0000

    completed step 1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,81 +1,41 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>{{ config('app.name') }}</title>
-        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-<<<<<<< HEAD
+<head>
+    <meta charset="UTF-8">
+    <title>{{ config('app.name') }}</title>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 
-=======
->>>>>>> e008cf0c40cebaac1add3aea0f7456b1131c7309
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-              integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-              crossorigin="anonymous" referrerpolicy="no-referrer" />
-<<<<<<< HEAD
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
 
-=======
->>>>>>> e008cf0c40cebaac1add3aea0f7456b1131c7309
-        <!-- Bootstrap 5 -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    </head>
-<<<<<<< HEAD
-
-    <body>
-        <!-- Bootstrap 5 Navbar -->
-        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-          <div class="container-fluid">
+<body>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div class="container-fluid">
             <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href=https://laravel.com/docs>Laravel Documentation</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href=https://laracasts.com/>Laravel Video Tutorials</a>
-              </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://laravel.com/docs">Laravel Documentation</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://laracasts.com/">Laravel Video Tutorials</a>
+                </li>
             </ul>
-          </div>
-        </nav>
-        
-        <div id="page-content-wrapper"> 
-            <div class="container-fluid"> 
-                <div class="row">
-                    <!-- Well for left side of the page (to compensate for widescreen) -->
-                    <div class="col-lg-2"></div>
-                    <!-- Slot for content from Lower Level views with sections called 'content' pulled in here -->
-                    <div class="col-lg-8"> @yield('content') </div>
-                    <!-- Well for right side of the page (to compensate for widescreen) -->
-                    <div class="col-lg-2"></div> 
-                </div> 
-            </div> 
-        </div> 
+        </div>
+    </nav>
 
-        <!-- Webpack mix npm generated -->
-        <link rel="stylesheet" href="{{asset('css/app.css')}}"> 
-        <script src="{{asset('js/app.js')}}"></script>
-        @stack('js_scripts')
-    </body>
+    <div id="page-content-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-8">@yield('content')</div>
+                <div class="col-lg-2"></div>
+            </div>
+        </div>
+    </div>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}"></script>
+    @stack('js_scripts')
+</body>
 </html>
-=======
-    <body>
-        <body> <nav class="navbar navbar-default navbar-static-top">
-           <ul class="nav navbar-nav">
-             <li><a href="{{ route('members.index') }}">Member List</a></li> 
-             <li><a href="{{ route('members.create') }}">New Member</a></li> 
-             <li><a href="{{ route('bookings.index') }}">Booking List</a></li> 
-            </ul>
-           </nav>
-            <div id="page-content-wrapper">
-               <div class="container-fluid"> 
-                <div class="row">
-                   <div class="col-lg-2"></div> 
-                   <div class="col-lg-8">
-                     @yield('content') 
-                    </div>
-                     <div class="col-lg-2"></div>
-                     </div>
-                     </div>
-                     </div>
-    </body>
-</html>
->>>>>>> e008cf0c40cebaac1add3aea0f7456b1131c7309
--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('products', App\Http\Controllers\ProductController::class);
 
-Route::resource('members', App\Http\Controllers\memberController::class);
+Route::resource('scorders', App\Http\Controllers\ScorderController::class);
 
+Route::resource('orderdetails', App\Http\Controllers\OrderdetailController::class);
 
-Route::resource('courts', App\Http\Controllers\courtController::class);
-
-
-Route::resource('bookings', App\Http\Controllers\bookingController::class);
+Route::get('product/displaygrid','App\Http\Controllers\ProductController@displayGrid')->name('products.displaygrid');
