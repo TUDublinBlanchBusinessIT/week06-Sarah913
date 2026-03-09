1c3f8699044849aab03504b2182a23ba1796ea61
Sarah Forde <you@example.com>
2026-03-09 17:21:09 +0000

    completed step 2

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,5 @@ Route::resource('scorders', App\Http\Controllers\ScorderController::class);
 Route::resource('orderdetails', App\Http\Controllers\OrderdetailController::class);
 
 Route::get('product/displaygrid','App\Http\Controllers\ProductController@displayGrid')->name('products.displaygrid');
+
+Route::get('product/additem/{id}','App\Http\Controllers\productController@additem')->name('products.additem');
